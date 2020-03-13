931507d407065dbe733a8819128824cf5dd202f5
Roman Storm <roman@peppersec.com>
2020-03-12 23:23:24 -0700

    toDecimals

--- src/DataGrid.tsx
+++ src/DataGrid.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import moment from 'moment'
 import {formatUserSummaryData} from '@aave/protocol-js'
-
+import {fromWei, toBN} from 'web3-utils'
 import logo from './logo.svg';
 import './App.css';
 import { useLiquidatorsQuery } from './apollo/generated';
 
 function getCurrentTimestamp(): number {
   return Number(moment().format('X'));
+}
+const toDecimal = (symbol: any, amount: any) => {
+  if(symbol === 'USDC') {
+    const a = toBN(amount).div(toBN(10).pow(toBN(6))).toString()
+    return a
+  } else {
+    return fromWei(amount)
+  }
 }
 
 export function DataGrid() {
@@ -34,9 +42,11 @@ export function DataGrid() {
       </p>
       <h3>READY</h3>
       {userReserves.map((userReserve, i) => (
-          <div key={i} style={{borderColor: "black"}}>
-          BORROWS
-          <p>{userReserve.reserve.symbol}: {userReserve.principalBorrows}</p>
+        <div key={i} style={{borderColor: "black"}}>
+          <hr></hr>
+          <p>ID: {userReserve.user.id}</p>
+          <b>BORROWS</b>
+          <p>{userReserve.reserve.symbol}: {toDecimal(userReserve.reserve.symbol, userReserve.principalBorrows)}</p>
           <h4>HF: {userReserve.user.healthFactor}</h4>
           COLLATERALS
           {userReserve.user.reservesData.map((res, ii) => (
